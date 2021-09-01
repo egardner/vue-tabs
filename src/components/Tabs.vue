a960c0a97f37b3fc10271ef3a79ebac24ca347e8
Eric Gardner <egardner@wikimedia.org>
2021-09-01 15:03:08 -0700

    Add more logic to Tabs.vue

--- src/components/Tabs.vue
+++ src/components/Tabs.vue
@@ -2,42 +2,84 @@
 	<div class="vue-tabs">
 		<div class="vue-tabs__header" role="tablist">
 			<div 
-				v-for="( tab, index ) in tabsData" 
-				:key="index"
-				role="tab"
-				@click="onClick( tab.id )"
-				:style="{ fontWeight: tab.isActive ? 'bold' : 'normal' }"
+				class="vue-tabs__header__list"
+				role="tablist"
+				tabindex="0"
+				:aria-activedescendant="currentTabId"
+				@keydown.right="next"
+				@keydown.down.prevent="next"
+				@keydown.left="prev"
+				@keydown.up.prevent="prev"
 			>
-				{{ tab.label }}
+				<div
+					v-for="( tab, index ) in tabsData"
+					:id="tab.id + '-label'"
+					:aria-selected="tab.id === currentTabId"
+					:aria-controls="tab.id"
+					class="vue-tabs__header__list__item"
+					:key="index"
+					role="tab"
+					tabindex="-1"
+					@click="select( tab.id )"
+					@keyup.enter="select( tab.id )"
+				>
+					{{ tab.label }}
+				</div>
 			</div>
 		</div>
 
 		<div class="vue-tabs__content">
 			<slot></slot>
 		</div>
+
+		<p>{{ currentTabIndex }}</p>
 	</div>
 </template>
 
 <script setup>
-import { reactive, provide, useSlots } from 'vue';
+/**
+ * Data for a single tab
+ *
+ * @typedef {Object} TabData
+ * @property {string} id
+ * @property {string} label
+ * @property {boolean} isActive
+ */
 
-// Define an optional "initialActive" prop, which must match the id of one of
-// the tabs. If this is not given, the initial active tab will be the first one.
+/**
+ * Map containing all data for all tabs, keyed by tab id.
+ *
+ * @typedef {Object.<string, TabData>} TabsData
+ */
+
+import {
+	computed,
+	provide,
+	reactive,
+	useSlots
+} from 'vue';
+
 const props = defineProps( {
+	/**
+	 * Must match the id of one of the tabs. If this prop is not provided, the
+	 * inital active tab will be the first one in the markup structure.
+	 */
 	initialActive: {
 		type: String,
 		required: false
 	}
 } );
 
-// Define the events that the component emits; in this case, a "tabchange"
-// event.
 const emit = defineEmits( [ 'tabchange' ] );
 
-// Construct the "tabs" data that will be exposed to the template and injected
-// to the child Tab components so that they can determine their appearance and
-// behavior. Slot content is always an array (of vdom nodes), but we want to
-// reduce this to a single keyed object for easier access to specific tabs.
+/**
+ * Data that will be exposed to the template and injected into the child Tab
+ * components so that they can determine their appearance and behavior. Slot
+ * content is always an array (of vdom nodes), but we want to reduce this to a
+ * single keyed object for easier access to specific tabs.
+ *
+ * @type {TabsData}
+ */
 const tabsData = reactive( useSlots().default().reduce( ( map, item, currentIndex ) => {
 	function isActive( item, currentIndex ) {
 		return props.initialActive ? 
@@ -56,11 +98,51 @@ const tabsData = reactive( useSlots().default().reduce( ( map, item, currentInde
 	return map;
 }, {} ) );
 
-// Handle click events and emit a "tabchange" event
-const onClick = ( id ) => {
-	Object.keys( tabsData ).forEach( id => { tabsData[ id ].isActive = false; } );
+const currentTabId = computed( () => {
+	return Object.values( tabsData ).find( ( tab ) => {
+		return tab.isActive === true;
+	} ).id;
+} );
+
+const currentTabIndex = computed( () => {
+	return Object.keys( tabsData ).findIndex( ( id ) => {
+		// currentTabId is a reactive object so we need to check its "value"
+		return id === currentTabId.value;
+	} );
+} );
+
+/**
+ * @param {string} id
+ * @emits tabchange
+ */
+function select ( id ) {
+	Object.keys( tabsData ).forEach( id => { tabsData[ id ].isActive = false; });
 	tabsData[ id ].isActive = true;
 	emit( 'tabchange', id );
+}
+
+/**
+ * Set the tab immediately after the currently active one as active
+ */
+function next () {
+	const ids = Object.keys( tabsData );
+	const nextIndex = currentTabIndex.value + 1
+
+	if ( ids.indexOf( nextIndex ) > -1 ) {
+		select( nextIndex );
+	}
+}
+
+/**
+ * Set the tab immediately before the currently active one as active
+ */
+function prev () {
+	const ids = Object.keys( tabsData );
+	const prevIndex = currentTabIndex.value - 1
+
+	if ( ids.indexOf( prevIndex ) > -1 ) {
+		select( prevIndex );
+	}
 }
 
 // Provide the tabsData object to the child Tab components
